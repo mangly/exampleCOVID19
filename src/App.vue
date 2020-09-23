6687e06897483917cad7aa8505949e22eec0a639
Hector Armando Rizo Blanco <elmangly@gmail.com>
2020-09-23 18:57:23 -0400

    option2 building

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,63 @@
 <template>
   <v-app>
-    <v-app-bar
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant.sync="mini"
       app
-      color="white"
-      height="80"
+      :expand-on-hover="expandOnHoverMethod()"
+    >
+      <v-img
+        src="./assets/rawImage.jpg"
+        gradient="to right, rgba(5, 11, 31, .8), rgba(5, 11, 31, .8)"
+        height="100%"
+      >
+        <v-list-item dark class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item>
+
+        <v-divider />
+
+        <v-list dark dense>
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-img>
+    </v-navigation-drawer>
+    <v-app-bar
+      elevation="0"
+      app
+      height="56"
       src="./assets/viggo-jorgen-p5N3PVOOF_0-unsplash (1).jpg"
     >
       <div class="d-flex align-center">
+        <v-btn icon @click.stop="changeMini">
+          <v-icon>{{iconChevron}}</v-icon>
+        </v-btn>
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
           src="./assets/30271657.png"
           transition="scale-transition"
-          width="200"
-        />
-
-        <!-- <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />-->
+        />
       </div>
 
-      <v-spacer />
+      <!-- <v-spacer /> -->
 
-      <div class="hidden-sm-and-down">
-        <v-btn href="/" text>
-          <span class="mr-2">Home</span>
-        </v-btn>
-        <v-btn href target="_blank" text>
-          <span class="mr-2">Data</span>
-        </v-btn>
-        <v-btn href target="_blank" text>
-          <span class="mr-2">Methodology</span>
-        </v-btn>
-        <v-menu offset-y bottom origin="center center" transition="scale-transition">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn text v-bind="attrs" v-on="on">
-              <span class="mr-2">Models</span>
-              <v-icon>mdi-chevron-down</v-icon>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item v-for="(item, i) in itemsModels" :key="i" @click>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <v-menu offset-y bottom origin="center center" transition="scale-transition">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn text v-bind="attrs" v-on="on">
-              <span class="mr-2">Resources</span>
-              <v-icon>mdi-chevron-down</v-icon>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item v-for="(item, i) in itemsResources" :key="i" @click>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <v-btn href target="_blank" text>
-          <span class="mr-2">Containment</span>
-        </v-btn>
-        <v-btn href target="_blank" text>
-          <span class="mr-2">News</span>
-        </v-btn>
-        <v-btn href target="_blank" text>
-          <span class="mr-2">About us</span>
-        </v-btn>
-      </div>
-      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer" />
     </v-app-bar>
-    <v-navigation-drawer color="grey" dark v-if="isMobile" v-model="drawer" absolute temporary>
-      <v-list dense>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main>
+    <v-main style="background-color:grey">
       <v-fade-transition mode="out-in">
         <router-view />
       </v-fade-transition>
@@ -109,51 +75,34 @@
 
 export default {
   name: "App",
-  drawer: null,
-
   // components: {
   //   HelloWorld
   // },
 
-  data: () => ({
-    drawer: false,
-    isMobile: false,
-    itemsModels: [
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me 2" }
-    ],
-    itemsResources: [
-      { title: "Methodology" },
-      { title: "Models" },
-      { title: "Simulations" },
-      { title: "State of the art in epidemis modelling" }
-    ],
-    items: [
-      { title: "Home", icon: "mdi-dashboard" },
-      { title: "About", icon: "mdi-question_answer" }
-    ]
-  }),
-
-  beforeDestroy() {
-    if (typeof window !== "undefined") {
-      window.removeEventListener("resize", this.onResize, { passive: true });
-    }
-  },
-
-  mounted() {
-    this.onResize();
-    window.addEventListener("resize", this.onResize, { passive: true });
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Home", icon: "mdi-home-city" },
+        { title: "My Account", icon: "mdi-account" },
+        { title: "Users", icon: "mdi-account-group-outline" }
+      ],
+      mini: false,
+      expandOnhover: false,
+      iconChevron: "mdi-chevron-left"
+    };
   },
 
   methods: {
-    onResize() {
-      if (window.innerWidth < 600) this.isMobile = true;
-      else {
-        this.isMobile = false;
-        this.drawer = false;
-      }
+    changeMini() {
+      this.mini = !this.mini;
+      this.expandOnhover = !this.expandOnhover;
+      this.iconChevron = this.mini ? "mdi-chevron-right" : "mdi-chevron-left";
+    },
+
+    expandOnHoverMethod() {
+      this.iconChevron = this.mini ? "mdi-chevron-right" : "mdi-chevron-left";
+      return this.expandOnhover;
     }
   }
 };
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,37 +1,41 @@
 <template>
   <section id="hero">
-    <v-img
-      :min-height="minHeight"
-      :src="require('@/assets/ciberseguridad-covid19-1200x630.jpg')"
-      class="white--text"
-      gradient="to right, rgba(5, 11, 31, .8), rgba(5, 11, 31, .8)"
-    >
-      <v-container class="fill-height px-4 py-12">
-        <v-responsive class="d-flex align-center" height="100%" max-width="700" width="100%">
-          <h1>Description</h1>
-          <br />
-          <p>
-            In this website you'll find information about the covid-19 pandemic, including data,
-            models, simulations, predictions, containment, measures, and epidemic literature.
-            This website is done by a group of researchers at Torus Actions in collaborations
-            with mathematicians at Max-Planck Institute for Mathematic in the Sciencies and
-            Czech Academy of Sciencies. This website in created May 2020 and willbe updated fast
-            and regulary, and will also show our own simulations and models for Covid-19.
-          </p>
+    <v-container>
+      <v-card>
+        <v-img
+          :min-height="minHeight"
+          :src="require('@/assets/ciberseguridad-covid19-1200x630.jpg')"
+          class="white--text"
+          gradient="to right, rgba(5, 11, 31, .8), rgba(5, 11, 31, .8)"
+        >
+          <v-container class="fill-height px-4 py-12">
+            <v-responsive class="d-flex align-center" height="100%" max-width="700" width="100%">
+              <h1>Description</h1>
+              <br />
+              <p>
+                In this website you'll find information about the covid-19 pandemic, including data,
+                models, simulations, predictions, containment, measures, and epidemic literature.
+                This website is done by a group of researchers at Torus Actions in collaborations
+                with mathematicians at Max-Planck Institute for Mathematic in the Sciencies and
+                Czech Academy of Sciencies. This website in created May 2020 and willbe updated fast
+                and regulary, and will also show our own simulations and models for Covid-19.
+              </p>
 
-          <div
-            :class="$vuetify.breakpoint.smAndDown ? 'flex-column align-start' : 'align-center'"
-            class="d-flex flex-wrap"
-          >
-            <!-- <base-btn>Discover More</base-btn>
+              <div
+                :class="$vuetify.breakpoint.smAndDown ? 'flex-column align-start' : 'align-center'"
+                class="d-flex flex-wrap"
+              >
+                <!-- <base-btn>Discover More</base-btn>
 
             <span class="font-weight-bold ml-6 mr-4 my-4">or</span>
 
-            <base-btn :ripple="false" class="pa-1" height="auto" text>Get Started Now</base-btn>-->
-          </div>
-        </v-responsive>
-      </v-container>
-    </v-img>
+                <base-btn :ripple="false" class="pa-1" height="auto" text>Get Started Now</base-btn>-->
+              </div>
+            </v-responsive>
+          </v-container>
+        </v-img>
+      </v-card>
+    </v-container>
   </section>
 </template>
 <script>
