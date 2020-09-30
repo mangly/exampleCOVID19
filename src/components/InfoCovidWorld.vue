b79b252ae3b58485315374176bc78442d8f1b96d
Hector Armando Rizo Blanco <elmangly@gmail.com>
2020-09-30 00:55:12 -0400

    building option2

--- src/components/InfoCovidWorld.vue
+++ src/components/InfoCovidWorld.vue
@@ -1,12 +1,25 @@
 <template>
-  <v-card class="pl-10 pr-10 pb-10 pt-10">
+  <v-card color="#000000" class="pl-3 pr-3 pt-2">
+    <div class="center1">
+      <div class="center2" style="font-size:30px">
+        <span style="color:#ffffff">#About</span>
+        <span style="color:#b70000">
+          <strong>COVID19</strong>
+        </span>
+        <span style="color:#ffffff">inTheWorld</span>
+      </div>
+    </div>
     <v-row>
       <v-col lg="3" cols="12">
-        <v-card dark color="#d0797c" class="mx-auto" height="100" width="100%">
+        <v-card class="mx-auto" dark color="#d0797c" height="100" width="100%">
           <v-card-text>
             <div>Total Confirmed</div>
             <v-divider></v-divider>
-            <p class="display-1 pt-2" style="color: white">{{infoCovidWorld.cases }}</p>
+            <div class="center1">
+              <div class="center2">
+                <p class="display-1 pt-2" style="color: white">{{infoCovidWorld.cases }}</p>
+              </div>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
@@ -15,11 +28,11 @@
           <v-card-text>
             <div>Active Cases</div>
             <v-divider></v-divider>
-            <p class="display-1 pt-2" style="color: white">
-              {{
-              infoCovidWorld.active
-              }}
-            </p>
+            <div class="center1">
+              <div class="center2">
+                <p class="display-1 pt-2" style="color: white">{{infoCovidWorld.active }}</p>
+              </div>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
@@ -28,16 +41,24 @@
           <v-card-text>
             <div>Total Recovered</div>
             <v-divider></v-divider>
-            <p class="display-1 pt-2" style="color: white">{{infoCovidWorld.recovered }}</p>
+            <div class="center1">
+              <div class="center2">
+                <p class="display-1 pt-2" style="color: white">{{infoCovidWorld.recovered }}</p>
+              </div>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
       <v-col lg="3" cols="12">
-        <v-card dark color="#000000" class="mx-auto" height="100" width="100%">
+        <v-card color="#eeeeee" class="mx-auto" height="100" width="100%">
           <v-card-text>
             <div>Total Deaths</div>
             <v-divider></v-divider>
-            <p class="display-1 pt-2" style="color: white">{{infoCovidWorld.deaths }}</p>
+            <div class="center1">
+              <div class="center2">
+                <p class="display-1 pt-2" style="color: black">{{infoCovidWorld.deaths }}</p>
+              </div>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
@@ -57,4 +78,11 @@ export default {
 </script>
 
 <style>
+.center1 {
+  display: flex;
+  align-items: center;
+}
+.center2 {
+  margin: 0 auto; /* requerido para alineación horizontal */
+}
 </style>
